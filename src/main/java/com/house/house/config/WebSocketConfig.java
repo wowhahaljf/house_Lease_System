be8420fac9e80c86d00209f0cf1276018dbdc90d
ljf <ljf@qq.com>
2022-12-10 21:51:48 +0800

    更新

--- src/main/java/com/house/house/config/WebSocketConfig.java
+++ src/main/java/com/house/house/config/WebSocketConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+
+
 @Configuration
 public class WebSocketConfig {
     @Bean
